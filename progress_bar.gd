cf68faf7f76a5d433f4ebd0bef1823d7a7f00694
kamisenin <senelle.camille18@gmail.com>
2026-05-23 11:59:59 +0200

    added the money text, still have some problems with the resize

--- progress_bar.gd
+++ progress_bar.gd
@@ -1,12 +1,12 @@
 extends Control
 
 @export var p_bar: ProgressBar
+@export var money_text : RichTextLabel
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	p_bar.value = 5.0
 	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -14,3 +14,7 @@ func _process(delta: float) -> void:
 
 func remove_star(remove: float) -> void:
 	p_bar.value -= remove
+
+func add_money(money : float) -> void:
+	money_text.value += money
+	money_text.update_text()
