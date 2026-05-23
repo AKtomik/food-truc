bb3d77b91aa81a5953b4a3ce9cf000621917523b
kamisenin <senelle.camille18@gmail.com>
2026-05-24 01:26:25 +0200

    added money label

--- scripts/furnitures/cash_machine.gd
+++ scripts/furnitures/cash_machine.gd
@@ -2,15 +2,21 @@ class_name CashMachine
 extends Node3D
 
 @export var starting_money: int = 100
+@export var label : Label3D
 var _money: int = 100
+var _max_digits : int = 15
 
 func _ready() -> void:
 	_money = starting_money
 	refresh()
 
 func refresh():
-	# update the 3D label
-	pass
+	var str_amount = "%d" % _money
+
+	if str_amount.length() > _max_digits:
+		label.text = str_amount.substr(0, _max_digits) + "..." + "$"
+	else:
+		label.text = "%.2f$" % _money + "$"
 
 func get_money() -> int:
 	return _money
--- scripts/main.gd
+++ scripts/main.gd
@@ -12,13 +12,16 @@ var jacket_chance : int = 80
 
 func _ready() -> void:
 	randomize()
-	generate_random_character(self)
+	generate_random_character(self, false)
 
 func _process(delta: float) -> void:
 	pass
 
 
-func generate_random_character(parent: Node3D) -> Node3D:
+func generate_food_queue() -> void :
+		pass
+
+func generate_random_character(parent: Node3D, critique : bool) -> Node3D:
 	var def = CharacterResource.new()
 	var random : RandomNumberGenerator = RandomNumberGenerator.new() 
 	
@@ -34,4 +37,4 @@ func generate_random_character(parent: Node3D) -> Node3D:
 		def.neck_mesh = mesh_library.neck_meshes.pick_random()
 	if (random.randi_range(1, 100) <= jacket_chance) :
 		def.jacket_mesh = mesh_library.jacket_meshes.pick_random()
-	return CharResourcesSpawner.spawn(def, parent, false, face_picker) # TODO gérer false ou true 
+	return CharResourcesSpawner.spawn(def, parent, critique, face_picker) # TODO gérer false ou true 
