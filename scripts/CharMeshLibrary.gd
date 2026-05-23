93c530cc2bd7460af291fc00b9ee98d893d6726a
kamisenin <senelle.camille18@gmail.com>
2026-05-23 18:37:44 +0200

    added procedural player generation, pushing to get objs

---
+++ scripts/CharMeshLibrary.gd
@@ -0,0 +1,9 @@
+class_name CharacterMeshLibrary
+extends Resource
+
+@export var hair_meshes: Array[Mesh] = []
+@export var hat_meshes: Array[Mesh] = []
+@export var eyes_meshes: Array[Mesh] = []
+@export var mouth_meshes: Array[Mesh] = []
+@export var neck_meshes: Array[Mesh] = []
+@export var jacket_meshes: Array[Mesh] = []
