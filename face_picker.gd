112084545b358f636515bb69d56a98a5b77bef2c
kamisenin <senelle.camille18@gmail.com>
2026-05-24 00:29:44 +0200

    Fixed face color problem using an invisible mask

--- face_picker.gd
+++ face_picker.gd
@@ -4,9 +4,11 @@ extends Node
 const face_bank: FaceBank = preload("res://resources/FaceBank.tres")
 
 static var culinary_critique_texture: Texture2D = null
+static var culinary_critique_color: Color
 
 func setup() -> void:
 	culinary_critique_texture = face_bank.faces.pick_random()
+	culinary_critique_color = CharResourcesSpawner.random_skin_color()
 
 func apply_critique_texture(face_mesh : MeshInstance3D) -> void:
 	_apply_texture(culinary_critique_texture, face_mesh)
@@ -17,4 +19,5 @@ func apply_random_texture(face_mesh : MeshInstance3D) -> void :
 func _apply_texture(texture: Texture2D, face_mesh : MeshInstance3D) -> void:
 	var mat = StandardMaterial3D.new()
 	mat.albedo_texture = texture
+	mat.transparency = BaseMaterial3D.TRANSPARENCY_ALPHA_SCISSOR
 	face_mesh.material_override = mat
