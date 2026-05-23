8fbe08ff1c7f7465066a419b2daf005e98d8e8e2
AKtomik <97125853+AKtomik@users.noreply.github.com>
2026-05-23 19:37:18 +0200

    food resources

---
+++ scripts/items/item_resource.gd
@@ -0,0 +1,4 @@
+extends Resource
+class_name ItemResource
+
+@export var model_scene: PackedScene
--- scripts/tools/cubishop.gd
+++ scripts/tools/cubishop.gd
@@ -1,7 +1,7 @@
 class_name Cubishop
 extends ItemTool
 
-@export var item_scene: PackedScene
+@export var item_resource: ItemResource
 @export var price: int
 @export var money_manager: CashMachine
 
@@ -12,11 +12,11 @@ func can_take(_item: Item) -> bool:
 	return money_manager.get_money()
 
 func put(_item: Item) -> bool:
-	push_error("should not be called here")
+	push_error("can't put anything in cubishop")
 	return false
 
 func take() -> Item:
 	money_manager.pay(price)
-	var item = item_scene.instantiate()
+	var item = item_resource.model_scene.instantiate()
 	add_child(item)
 	return item
