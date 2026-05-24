19362b952fa824fe5ab7561c44be0b5176b6721f
kamisenin <senelle.camille18@gmail.com>
2026-05-24 15:46:14 +0200

    added automatique type writer

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,8 +1,11 @@
 class_name GameManager
 extends Node3D
 
+@export var typewriter_label : TypeWriter
+
 func _ready() -> void:
 	randomize()
+	typewriter_label.display("Eh, c'est moi. Écoute bien.")
 
 func _process(_delta: float) -> void:
 	pass
