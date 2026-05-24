bb8b14802819542eebb41bdbeea0f7319acac739
AKtomik <97125853+AKtomik@users.noreply.github.com>
2026-05-24 09:41:21 +0200

    order ui

--- scripts/orders/order.gd
+++ scripts/orders/order.gd
@@ -6,12 +6,20 @@ var resource: OrderResource
 var is_inspector: bool
 var time_max: float
 var time_remain: float
+var number: int
+
+@export var ui_image: TextureRect
+@export var ui_label: Label
 
 func setup(order_resource: OrderResource, inspection: bool = false, time_factor: int = 1):
 	resource = order_resource
 	time_max = order_resource.expiration_time * time_factor
 	time_remain = time_max
 	is_inspector = inspection
+	number = randi() % 10000
+
+	ui_image.texture = order_resource.kaway_image
+	ui_label.text = "#"+str(number)
 
 func given_happy(food: Item) -> bool:
 	if (resource.good_food.is_scene(food)):
