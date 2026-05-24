232f2d57e4d94ea84a682fb48ab791ac02b1dadb
AKtomik <97125853+AKtomik@users.noreply.github.com>
2026-05-24 06:11:45 +0200

    scene and scripts restruct

--- scripts/star_manager.gd
+++ scripts/star_manager.gd
@@ -1,11 +1,16 @@
-extends MeshInstance3D
+class_name StarManager
+extends Control
 
+@export var p_bar: ProgressBar
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	p_bar.value = 5.0
 	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func remove_star(remove: float) -> void:
+	p_bar.value -= remove
