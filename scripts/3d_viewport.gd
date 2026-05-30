d5e662a953eaebd562b55277e10be3f32dd72e80
AKtomik <97125853+AKtomik@users.noreply.github.com>
2026-05-30 11:53:14 +0200

    ui orders is web compatible

--- scripts/3d_viewport.gd
+++ scripts/3d_viewport.gd
@@ -6,6 +6,10 @@ extends MeshInstance3D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	#subviewport.set_clear_mode(SubViewport.CLEAR_MODE_ONCE)
-	#await get_tree().process_frame
-	#await get_tree().process_frame
-	viewportquad.material_override.albedo_texture = subviewport.get_texture()
+	subviewport.set_clear_mode(SubViewport.CLEAR_MODE_ALWAYS)# for web
+	await RenderingServer.frame_post_draw
+	await get_tree().process_frame
+	await get_tree().process_frame    
+	var mat = viewportquad.material_override.duplicate()
+	mat.albedo_texture = subviewport.get_texture()
+	viewportquad.material_override = mat
